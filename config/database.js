a9b4ccd3e024d24dfd43bf42367d3f642d0e936d
Stuart R. Kirk <20864330+redhatstuart@users.noreply.github.com>
2024-08-29 12:12:43 -0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongodbUri = process.env.MONGODB_URI || 'mongodb://localhost/meanstacktutorials';
 
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    remoteUrl : 'mongodb://demo-apex-m02.rhhc.wwtatc.com:27017/node-todo',
     localUrl: mongodbUri
 };
